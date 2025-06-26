908c3b1af66d407a7e4faa4d68df2a8374803e7f
David Michaels <dmichaels@gmail.com>
2025-06-26 18:32:55 -0400

    etc

--- CellGridView/Sources/CellGridView/CellGridView+Config.swift
+++ CellGridView/Sources/CellGridView/CellGridView+Config.swift
@@ -3,46 +3,6 @@ import Utils
 
 extension CellGridView
 {
-    public struct Defaults
-    {
-        // The size related properties here (being outward facing) are unscaled.
-
-        public static let viewBackground: Colour = Colour.darkGray
-        public static let viewTransparency: UInt8 = Colour.OPAQUE
-        public static let viewScaling: Bool = true
-
-        public static let cellSize: Int = 25
-        public static let cellPadding: Int = 1
-        public static let cellShape: CellShape = CellShape.rounded
-        public static let cellColor: Colour = Colour.white
-
-        public static let cellSizeMax: Int = 200
-        public static let cellSizeInnerMin: Int = 1
-        public static let cellPaddingMax: Int = 8
-
-        public static let gridColumns: Int = 0
-        public static let gridRows: Int = 0
-
-        public static let cellAntialiasFade: Float = 0.6  // smaller -> smoother
-        public static let cellRoundedRadius: Float = 0.25
-        public static let restrictShift: Bool = true
-        public static let unscaledZoom: Bool = false
-
-        public static let selectMode: Bool = true
-        public static let automationMode: Bool = true
-        public static let automationInterval: Double = 0.5
-
-        // Only used on CellGridView.initialize.
-        //
-        public static let cellSizeFit: Bool = false
-        public static let cellPreferredSizeMarginMax: Int = 30
-        public static let centerCells: Bool = false
-
-        // Wraparound support is incomplete, half-baked, and of questionable utility.
-        //
-        public static let gridWrapAround: Bool = false
-    }
-
     open class Config
     {
         public var viewBackground: Colour
